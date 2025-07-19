f1709daa59babc6b0d5be491cbda00577200c52f
Praptee12 <080bct50@ioepc.edu.np>
2025-07-19 08:40:14 +0545

    Add changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useEffect ,useState}from 'react';
 import Navbar from '../components/Navbar';
 import Card from '../components/card';
+import axios from 'axios';
 const Home = () => {
+  const[blogs,setBlogs]=useState([])
+  async function fetchBlogs(){
+    const response= await axios.get("https://687af3c4abb83744b7ee4a32.mockapi.io/blogs")
+    // console.log(response.data)
+    console.log(response)
+    if(response.status===200){
+      setBlogs(response.data)
+    }
+      else{
+        alert("Error ayoooo !!")
+      }
+    }
+
+    //  setBlogs(response.data)
+  
+  useEffect(function(){ 
+    fetchBlogs()
+// anonymous/callback function
+  },[]) 
+  console.log(blogs,"This is blogs")  //dependent Array//
   return (
-    <div>
+   <>
       <Navbar/>
-      <Card/>
-    </div>
+      <div className="flex justify-between flex-wrap">
+        {blogs.map(function(blog){
+          return(   
+          <Card blog={blog}/>
+          )
+        
+         })}
+      </div>
+      
+   </>
   );
 };
-
 export default Home;
