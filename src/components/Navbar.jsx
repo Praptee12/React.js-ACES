7e991e66e46f116d94464160c147576cdd9ee5da
Praptee12 <080bct50@ioepc.edu.np>
2025-07-21 14:14:01 +0545

    Add changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="bg-[#f9f1f0] ">
+    <div className="bg-[#4c3e3c] ">
     <nav className=" block bg-[#fadcd9]  shadow-md fixed w-full top-0 z-50 mb-20">
       <div className="max-w-7xl mx-auto px-2  sm:px-6 lg:px-8 py-2 flex justify-between items-center">
         {/* Logo */}
